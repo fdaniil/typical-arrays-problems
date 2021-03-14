2e8c301085ad8f6fe3611c42a75fffc243b8f672
fdaniil <frolov.dan.mail@gmail.com>
2021-03-15 00:48:50 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array||!array.length) {
+        return 0;
+    }
+    let min = array[0];
+    array.forEach(element => {
+        if(element < min)
+            min = element;
+    });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array||!array.length) {
+        return 0;
+    }
+    let max = array[0];
+    array.forEach(element => {
+        if(element > max)
+            max = element;
+    });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array||!array.length) {
+        return 0;
+    }
+    let sum = 0;
+    let n = 0;
+    array.forEach(element => {
+        sum += element;
+        n++;
+    });
+  return sum/n;
 }
